9c240c9f7b2f491ab55459665cb91fd37c7b4c01
Gabriel Santos <gabrielsantoscruz61@gmail.com>
2025-04-19 09:21:28 -0300

    :recycle: fix(Repositories): alterações nos repositórios

--- app/backend/controller/grade/GradeRepository.ts
+++ app/backend/controller/grade/GradeRepository.ts
@@ -1,5 +1,22 @@
 import { Grade } from "../../core/model/Grade";
+import { PrismaClient } from "@prisma/client";
 
 export default class GradeRepository {
+    private static db = new PrismaClient
 
+    static async getAllGradesBySubjectId(subjectId: number): Promise<Grade[]> {
+        try {
+            const grades = await this.db.grade.findMany({
+                where: {
+                    subjectId: subjectId
+                }
+            })
+            return grades
+        } catch (error) {
+            console.error("Error when searching for subjects: ", error)
+            throw error
+        }
+
+
+    }
 }
--- app/backend/controller/user/UserRepository.ts
+++ app/backend/controller/user/UserRepository.ts
@@ -5,28 +5,48 @@ export default class UserRepository {
     private static db = new PrismaClient
 
     static async getAllUsers(): Promise<User[]> {
-        return await this.db.user.findMany()
+        try {
+            return await this.db.user.findMany()
+        } catch(error) {
+            console.error("Error when searching users: ", error)
+            throw error
+        }
     }
 
     static async getUserById(id: number): Promise<User> {
-        const user = await this.db.user.findUnique({
-            where: {id},
-        })
-
-        return user as User
+        try{
+            const user = await this.db.user.findUnique({
+                where: {id},
+            })
+    
+            return user as User
+        } catch(error) {
+            console.error("Error when searching user: ", error)
+            throw error
+        }
     }
 
     static async saveUser(user: User): Promise<User> {
-        return await this.db.user.upsert({
-            where: {id: user.id},
-            update: user,
-            create: user
-        })
+        try{
+            return await this.db.user.upsert({
+                where: {id: user.id},
+                update: user,
+                create: user
+            })
+        } catch(error) {
+            console.error("Error when save user: ", error)
+            throw error
+        }
     }
 
     static async deleteUser(id: number): Promise<void> {
-        await this.db.user.delete({
-            where: { id },
-        })
+        try {
+            await this.db.user.delete({
+                where: { id },
+            })
+        } catch(error) {
+            console.error("Error when delete user: ", error)
+            throw error
+        }
     }
 }
