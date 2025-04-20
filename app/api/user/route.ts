5995929f344aa43900a9256cdac6fd6c8e17f4d9
Gabriel Santos <gabrielsantoscruz61@gmail.com>
2025-04-19 22:25:58 -0300

    :recycle: fix(refatoraction)

--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -3,35 +3,41 @@ import UserRepository from '@/app/backend/controller/user/UserRepository'
 import { signupSchema } from '@/app/backend/schema/signup'
 
 export async function POST(req: Request) {
-  const body = await req.json()
+    const body = await req.json()
 
-  const saveData = signupSchema.safeParse(body)
-  if(!saveData.success){
-    return NextResponse.json({ error: saveData.error.flatten().fieldErrors })
-  }
+    try {
+        const saveData = signupSchema.safeParse(body)
+        if (!saveData.success) {
+            return NextResponse.json({ error: saveData.error.flatten().fieldErrors })
+        }
+        
+        const hasEmail = await UserRepository.getUserByEmail(body.email)
+        if (hasEmail) {
+            return NextResponse.json({ message: "Email already registered." }, { status: 409 })
+        }
 
-  try {
+        const user = await UserRepository.createUser(body)
 
-    const hasEmail = await UserRepository.getUserByEmail(body.email)
+        return NextResponse.json(user, { status: 201 })
 
-    if (hasEmail) {
-      return NextResponse.json({ message: "Email já cadastrado!" }, { status: 409 })
+    } catch (error) {
+        console.error(error)
+        return NextResponse.json(
+            { message: "Error when creating users." },
+            { status: 500 }
+        )
     }
-
-    const user = await UserRepository.createUser(body)
-
-    return NextResponse.json(user, { status: 201 })
-
-  } catch(error){
-    console.error(error)
-    return NextResponse.json(
-      { message: "Erro ao criar usuário." },
-      { status: 500 }
-    )
-  }
 }
 
 export async function GET() {
-  const users = await UserRepository.getAllUsers()
-  return NextResponse.json(users)
+    try{
+        const users = await UserRepository.getAllUsers()
+        return NextResponse.json(users, { status: 200 })
+    } catch (error) {
+        console.error(error)
+        return NextResponse.json(
+            { message: "Errer when searching users."},
+            { status: 500}
+        )
+    }
 }
--- app/backend/schema/signup.ts
+++ app/backend/schema/signup.ts
@@ -1,7 +1,7 @@
 import { z } from 'zod';
 
 export const signupSchema = z.object({
-    name: z.string({ message: 'Nome é obrigatório' }).min(4, 'Seu nome precisa de no mínimo 2 caractéres'), // string de no mínimo 2 caractéres
-    email: z.string({ message: 'Email é obrigatório' }).email({ message: 'Por favor, digite um email válido' }), // valida se é um email correto (com @ e tals)
-    password: z.string({ message:'Senha é obrigatória' }).min(8, 'Sua senha precisa ter 4 caractéres')
+    name: z.string({ message: 'Name is mandatory' }).min(4, 'Your name needs at least 4 characters'), // string de no mínimo 2 caractéres
+    email: z.string({ message: 'Email is mandatory' }).email({ message: 'Please, type a valid email' }), // valida se é um email correto (com @ e tals)
+    password: z.string({ message:'Password is mandatory' }).min(8, 'Your password must be at least 4 characters long')
 })
